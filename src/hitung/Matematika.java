29ac7589ac740ad66967ed08ee01159c7ed487a2
Syauqiqh <shauqikhairi@gmail.com>
2023-06-12 15:36:36 +0800

    membuat constroctor dan setPenjumlahan

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author Asus
- */
 public class Matematika {
-    
+    //variable yang akan dibuat
+    double bil1,bil2;
+
+    //constructor
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    //method penjumlahan
+    public double setPenjumlahan(){
+        return bil1 + bil2;
+    }
 }
