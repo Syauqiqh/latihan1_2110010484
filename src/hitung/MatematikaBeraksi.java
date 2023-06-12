6c7df3a3e2aadf72353fcecf540401a0428dd9e6
Syauqiqh <shauqikhairi@gmail.com>
2023-06-12 15:49:12 +0800

    membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author Asus
- */
+
 public class MatematikaBeraksi {
     
+    public static void main(String[] args) {
+        Matematika syauqi = new Matematika(8,4);
+        System.out.println("Hasil Penjumlahan : "+syauqi.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : "+syauqi.setPengurangan());
+        System.out.println("Hasil Perkalian : "+syauqi.setPerkalian());
+        System.out.println("Hasil Pembagian : "+syauqi.setPembagian());
+    }
 }
